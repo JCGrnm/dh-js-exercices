778949a35456f6acc26213b28623b76a7ab897d2
Jonna C. Geronimo <jonnacgeronimo@gmail.com>
2023-05-08 12:31:34 +0200

    fix: fix parenthesis

--- 3-arrow-functions/exercise-3.js
+++ 3-arrow-functions/exercise-3.js
@@ -1,16 +1,11 @@
-// # Arrow functions - Exercise 3
+const sum = (a, b) => {return a + b};
 
-// Turn the variables functions into arrow functions and combine the functions so that the result of the following mathematical operation is printed on the console: `((2 + 4) * (5 + 2) - 2) / 5`
+const subtract = (a, b) => {return a - b};
 
-const sum = (a, b) => a + b;
+const multiply = (a, b) => {return a * b};
 
-const subtract = (a, b) => a - b;
+const divide = (a, b) => {return a / b};
 
-const multiply = (a, b) => a * b;
-
-const divide = (a, b) => a / b;
-
-const log = (value) => console.log(value);
+const log = (value) =>{return console.log(value)};
 
 log(divide(subtract(multiply(sum(2,4), sum(5,2)), 2), 5))
-
